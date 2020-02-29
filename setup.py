2e2433ae201156bc2780acfa780be4e2b4a83922
LivingWithHippos <salabracco91@gmail.com>
2020-02-29 15:16:32 +0100

    Added pipfile and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="unchained-bot-LivingWithHippos",
+    version="0.1",
+    description="Telegram Bot for Real-Debrid.",
+    url="https://github.com/LivingWithHippos/unchained-bot",
+    author="LivingWithHippos",
+    author_email="",
+    install_requires=[
+        "python-telegram-bot",
+        "requests",
+    ],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4'
+)
