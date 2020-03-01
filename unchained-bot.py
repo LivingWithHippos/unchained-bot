24994784aeb16f32b2f52a1690e4dd4aa2f1b186
LivingWithHippos <salabracco91@gmail.com>
2020-03-01 13:58:00 +0100

    Added logger

--- unchained-bot.py
+++ unchained-bot.py
@@ -34,6 +34,7 @@ def load_bot():
             dispatcher = updater.dispatcher
             logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                                 level=logging.INFO)
+            logger = logging.getLogger(__name__)
     else:
         print("Missing bot token file: " + bot_config_path)
         exit(1)
