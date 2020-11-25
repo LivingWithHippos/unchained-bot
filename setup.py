242c17318b253b2fda920dcbf05520f5692a06c2
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-11-25 10:20:08 +0100

    upped minimum python version
    needed for bot library >=13

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.4'
+    python_requires='>=3.6'
 )
